7a3fc4b665362fed448ec0c7953e7e81b2053a24
Dodor15 <lerka.elizarova@bk.ru>
2022-12-01 10:26:44 +0500

    Start

---
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,28 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "CrimeListFragment"
+class CrimeListFragment : Fragment() {
+
+    private lateinit var crimeRecyclerView:
+            RecyclerView
+
+    private val crimeListViewModel:
+            CrimeListViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes:${crimeListViewModel.crimes.size}")
+    }
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+}
